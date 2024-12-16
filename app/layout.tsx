565f45ec25c9f9cb382b0100b11fbf75515eff7d
alvhursa1 <alvaro.hurtado.s@me.com>
2024-12-16 00:30:43 -0500

    v6.1 rionegro

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/next';
+
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <Analytics />
+      </body>
     </html>
   );
 }
